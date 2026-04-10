f41b85b8576d194d448f8aed92b97958016066f5
datcaodev <datcaodev@gmail.com>
2026-04-10 15:40:32 +0700

    fix

--- dist/constants/regex.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.phoneRegex = void 0;
-exports.phoneRegex = /^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$|^0(2[0-9]{1,2})[0-9]{7}$/;
-//# sourceMappingURL=regex.js.map
